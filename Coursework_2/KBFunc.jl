9584da00b9228b1e40abb00c2a1d9203e045cbee
os416 <os416@cam.ac.uk>
2023-12-29 12:08:25 +0000

    Typesetting and changes to how data is inputted

--- Coursework_2/KBFunc.jl
+++ Coursework_2/KBFunc.jl
@@ -1,13 +1,18 @@
 using Printf
 using Statistics
 
-function KBF(args...)
-    if (sum(args) >= 15*length(args)/2) | (prod(args) <=0.75)
-        return Inf
-    else
-        cosargs =  broadcast(cos, args)
-        num =  sum(cosargs .^4) - 2prod(cosargs .^2)
-        denom = sqrt(sum([index*num^2 for (index, num) in enumerate(args)]))
-        return num/denom
+function KBF(points::Vector{Vector{Float16}})
+    cost = Float64[]
+    for x in points
+        if (sum(x) >= 15*length(x)/2) | (prod(x) <=0.75)
+            push!(cost, Inf64)
+        else
+            cosargs =  broadcast(cos, x)
+            num =  sum(cosargs .^4) - 2prod(cosargs .^2)
+            denom = sqrt(sum([index*num^2 for (index, num) in enumerate(x)]))
+            push!(cost, num/denom)
+        end
     end
+    return cost
 end
+
--- Coursework_2/Main.jl
+++ Coursework_2/Main.jl
@@ -7,12 +7,7 @@ include("KBFunc.jl")
 
 x_1 = x_2 = LinRange(0, 10, 1000)
 pop_grid = [0 for i in x_1, j in x_2]
-z = [KBF(i, j) for i in x_1, j in x_2]
 
-contourf(x, y, z, plot_title="Contour Plot", camera=(180, 30), color=:turbo)
+contourf(x_1, x_2, KBF, plot_title="Contour Plot", camera=(180, 30), color=:turbo)
 
 
-struct GA
-    obj_func_iter::Int16
-    
-end
