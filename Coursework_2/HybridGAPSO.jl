e8ade0a6d68d7186ebd8ae35e8186059686d524f
os416 <os416@cam.ac.uk>
2024-01-04 17:19:31 +0000

    Implemented archiving for PSO

--- Coursework_2/HybridGAPSO.jl
+++ Coursework_2/HybridGAPSO.jl
@@ -11,7 +11,7 @@ include("ParticleSwarm.jl")
 
 function GAPSO(
                 dim::Int, pop_size::Int, innertia::Float64, phi_p::Float64,
-                phi_g::Float64, p_c::Float64, p_m::Float64, crossover, scoring,
+                phi_g::Float64, p_c::Float64, p_m::Float64, crossover,
                 plots::Bool
                 )
     iterations = floor(Int, 10000/pop_size)
@@ -23,19 +23,19 @@ function GAPSO(
     gapopu = GA_Popul(pop_size, dim)
     swarm.pos = mapreduce(permutedims, vcat, gapopu.positions)
 
-    scorings = Float64[scoring(swarm.val)]
+    archive_scorings = Float64[score_top1(swarm.val)]
+    swarm1_scorings = Float64[score_top1(swarm.val)]
+    swarm10_scorings = Float64[score_top10(swarm.val)]
     contscatplot(eachrow(swarm.pos), range, objfunc, string(0), plots)
-    contscatplot(gapopu.positions, range, objfunc, string(0.0), plots)
 
     for iter in 1:iterations
-        if 0 < iter % 4 <= 2
+        if 0 < iter % 10 <= 5
             swarm = update_velocity(swarm)
             swarm = update_positions(swarm)
             swarm = update_pos_archives(swarm)
             swarm = update_hparams(swarm, iterations)
             vec_pos = [row[:] for row in eachrow(swarm.pos)]
             gapopu = GA_Popul(pop_size, dim, vec_pos, swarm.val)
-            push!(scorings, scoring(swarm.val))
         else
             swarm = update_velocity(swarm)
             gapopu = single_iteration(gapopu, crossover, p_c, p_m)
@@ -43,16 +43,21 @@ function GAPSO(
             swarm.val_temp = gapopu.scores
             swarm = update_pos_archives(swarm)
             swarm = update_hparams(swarm, iterations)
-            push!(scorings, scoring(gapopu.scores))
         end
+
+        push!(archive_scorings, score_top1(vec(swarm.val_archive)))
+        push!(swarm1_scorings, score_top1(swarm.val))
+        push!(swarm10_scorings, score_top10(swarm.val))
+
         if (iter % 10 == 0) | (iter in 1:10)
             contscatplot(eachrow(swarm.pos), range, objfunc, string(iter), plots)
         end
     end 
 
-    plot(0:iterations, scorings)
-    savefig("Figures\\f.png") 
+    contscatplot(eachrow(swarm.pos_archive), range, objfunc, "archive", plots)
+    plot(0:iterations, [archive_scorings, swarm1_scorings, swarm10_scorings])
+    savefig("Figures\\f_sum.png") 
 
-    return scorings[end]
+    return [archive_scorings[end], swarm1_scorings[end], swarm10_scorings[end]]
 end
 
