5e0fa5dcbf0726075b2ca942184461c41a19b94e
os416 <os416@cam.ac.uk>
2024-01-05 21:49:23 +0000

    Further fixes from before

--- Coursework_2/tests.jl
+++ Coursework_2/tests.jl
@@ -6,29 +6,29 @@ using LinearAlgebra
 using Random
 
 include("KBFunc.jl")
-include("GeneticAlgo.jl")
+#include("GeneticAlgo.jl")
 include("Misc.jl")
-include("HybridGAPSO.jl")
-include("ParticleSwarm.jl")
+#include("HybridGAPSO.jl")
+#include("ParticleSwarm.jl")
 
 println("test")
 
-@benchmark GA(2, 200, 0.7, 0.01, var_locus_crossover, false)
+#@benchmark GA(2, 200, 0.7, 0.01, var_locus_crossover, false)
 PGAPSO(8, 120, 0.8, 2.5, 1.25, 0.7, 0.01, var_locus_crossover, false)
 
-scores = []
-for iter in 1:50
-    push!(scores, PGAPSO(8, 120, 0.8, 2.5, 1.25, 0.7, 0.001, 
-                         var_locus_crossover, false))
-end
-println(mean(scores))
-println(std(scores))
+# scores = []
+# for iter in 1:50
+#     push!(scores, PGAPSO(8, 120, 0.8, 2.5, 1.25, 0.7, 0.001, 
+#                          var_locus_crossover, false))
+# end
+# println(mean(scores))
+# println(std(scores))
 
 
 #@benchmark GA(2, 200, 0.7, 0.01, var_locus_crossover, false)
 
-#PGAPSO(8, 200, 0.8, 2.5, 1.25, 0.7, 0.5, var_locus_crossover, false)
-#@benchmark PSO(2, 120, 0.8, 2.5, 1.25, false)
+#PSO(8, 200, 0.8, 2.5, 1.25, 0.7, 0.5, var_locus_crossover, false)
+#PSO(2, 120, 0.8, 2.5, 1.25, true)
 
 
 #rand(LinRange(-0.5, 1., 1000), 5, 2)[1, :]' .+ rand(LinRange(-0.0, 0.0, 1000), 5, 2)
